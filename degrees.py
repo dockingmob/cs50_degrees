0d8ae4a09d945a852641882c803a7fe14fbe3d6c
Manasseh Sarsure-Sadik <manassehsadik167@gmail.com>
2024-03-25 10:39:54 +0000

    Used the code from Git yet to rewrite

--- degrees.py
+++ degrees.py
@@ -91,9 +91,42 @@ def shortest_path(source, target):
 
     If no possible path, returns None.
     """
+    num_explored = 0
 
-    # TODO
-    raise NotImplementedError
+    start = Node(state=source, parent=None, action=None)
+    frontier = QueueFrontier()
+    frontier.add(start)
+
+    # Set of explored actors
+    explored = set()
+
+    while True:
+        # If nothing left in frontier, then no path
+        if frontier.empty():
+            return None
+
+        node = frontier.remove()
+        #print(f"Exploring {node.state}")
+        num_explored += 1
+
+        # Mark actor (node) as explored
+        explored.add(node.state)
+        # Find the neighbors (actors to which he can connect) of the actor
+        neighbors = neighbors_for_person(node.state)
+        for movie, actor in neighbors:
+            if actor not in explored and  not frontier.contains_state(actor):
+                child = Node(state=actor, parent=node, action=movie)
+                if child.state == target:
+                    # Return list of tuples (movie_id, actor_id)
+                    path = []
+                    node = child
+                    while node.parent is not None:
+                        path.append((node.action, node.state))
+                        node = node.parent
+
+                    path.reverse()
+                    return path
+                frontier.add(child)
 
 
 def person_id_for_name(name):
